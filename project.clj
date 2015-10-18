e98e9bae9db15098d34f0d537d3fff8ed97e3235
Roger Tawa <roger@tawacentral.net>
2015-10-17 22:58:41 -0400

    Minor fixes to project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject bowling "0.1.0"
   :description "Bowling score keeper, used as playground for learning clojure."
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ^:skip-aot bowling.core
   :target-path "target/%s"
